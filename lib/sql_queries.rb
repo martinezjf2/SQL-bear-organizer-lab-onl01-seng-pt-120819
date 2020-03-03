6ec10bcf2839d2a62127620d2774c6bb62a29be4
Jeffrey Martinez <martinezjf2@gmail.com>
2020-03-03 14:02:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,10 +27,10 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears"
+  "SELECT * FROM bears WHERE (gender, temperament) = ('M', aggressive)"
 end
 
-
+# (8,null, 20, "M", "black", "aggressive", 0);
 
     # -- ORDER OF OPERATIONS
 
